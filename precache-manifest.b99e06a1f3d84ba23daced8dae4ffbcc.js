d210a56bce64a97a7420e9dc515770a2d6161043
Jacob Kedar Krevat <jacobtie@gmail.com>
2020-05-10 12:51:48 -0400

    Updates

---
+++ precache-manifest.b99e06a1f3d84ba23daced8dae4ffbcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a0d8b70812417a437b0b44ca3aa434",
+    "url": "/madlibs-ui/index.html"
+  },
+  {
+    "revision": "977ab62b827207c2b123",
+    "url": "/madlibs-ui/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "977ab62b827207c2b123",
+    "url": "/madlibs-ui/static/js/2.8ed167c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/madlibs-ui/static/js/2.8ed167c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a7fd3f078295a46943",
+    "url": "/madlibs-ui/static/js/main.041487a9.chunk.js"
+  },
+  {
+    "revision": "8ec41787866d76681675",
+    "url": "/madlibs-ui/static/js/runtime-main.768aa216.js"
+  }
+]);
